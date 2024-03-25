5e98d4a92f4a2e39e6db42c9e74aaf081607ea4e
universmc <hubmaster74@gmail.com>
2024-03-25 20:50:46 +0100

    SessionStart

---
+++ model/SinglePage/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+	mkdir -p build build/tmp data data/train data/val data/work models src
+	mkdir -p src/html src/css src/js src/json src/svg src/sass src/sh
+	touch src/html/wirefram.html src/css/stylus.css src/js/scripts.js src/json/layout.json src/svg/logo.svg src/sass/dev-stylus.scss
