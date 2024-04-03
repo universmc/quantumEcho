cb776704a5422deda0f57f5a0900f45af5d72015
universmc <hubmaster74@gmail.com>
2024-04-03 10:12:39 +0200

    SessionStart

---
+++ Makefile/output/js_20240403075342.301.js
@@ -0,0 +1,23 @@
+Je suis heureux de vous aider. Voici le code JavaScript que vous pouvez utiliser pour afficher un message dans la console lors du chargement de la page d'index :
+
+```javascript
+console.log("La page d'index a été chargée avec succès !");
+```
+
+Pour que ce code fonctionne, vous devez inclure la balise `<script>` dans la section `<head>` ou `<body>` de votre fichier HTML, comme ceci :
+
+```html
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Ma page d'index</title>
+  <script src="script.js"></script>
+</head>
+<body>
+  <!-- Le contenu de votre page -->
+</body>
+</html>
+```
+
+Assurez-vous que le fichier JavaScript `script.js` est bien placé dans le même dossier que votre fichier HTML pour que la balise `<script>` puisse le trouver et l'inclure correctement.
---
+++ Makefile/output/js_20240403075501.539.js
@@ -0,0 +1,9 @@
+Je suis heureux de vous aider ! Voici le code JavaScript demandé pour afficher un message dans la console lors du chargement de la page d'index :
+
+```javascript
+window.onload = function() {
+    console.log("La page a bien été chargée !");
+}
+```
+
+Ce code utilise l'événement `onload` de la fenêtre pour exécuter une fonction une fois que la page a fini de se charger. Cette fonction appelle la méthode `console.log` pour afficher le message "La page a bien été chargée !" dans la console.
