b1c0725a1a49e34e7b744fc9cf65fed31fdb45ce
universmc <hubmaster74@gmail.com>
2024-04-03 09:52:51 +0200

    SessionStart

---
+++ Makefile/output/js_20240403073644.407.js
@@ -0,0 +1,9 @@
+Je suis heureux de vous aider. Voici le code JavaScript à inclure dans votre fichier script.js pour afficher un message dans la console lors du chargement de la page d'index :
+
+```javascript
+window.onload = function() {
+  console.log("La page a été chargée avec succès !");
+}
+```
+
+Ce code utilise l'événement `onload` du `window` pour exécuter une fonction lorsque la page a fini de se charger. La fonction affiche ensuite un message dans la console à l'aide de la méthode `console.log()`.
---
+++ Makefile/output/js_20240403073806.317.js
@@ -0,0 +1,9 @@
+Je suis heureux de vous aider ! Voici le code JavaScript demandé pour afficher un message dans la console lors du chargement de la page d'index :
+
+<script>
+window.onload = function() {
+console.log("Page index chargée avec succès !");
+}
+</script>
+
+Ce code utilise l'événement onload de la fenêtre pour exécuter une fonction JavaScript lorsque la page a fini de se charger. Cette fonction appelle simplement la méthode console.log() pour afficher un message dans la console du navigateur.
